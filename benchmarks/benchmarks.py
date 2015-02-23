d1b692646af5c3a1a5e08c5c73c28c7e4e81e625
arkilic <arkilic@bnl.gov>
2015-02-23 11:20:37 -0500

    WIP: insert_beamlineconfig test run success. More tests to come...

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -3,6 +3,7 @@
 
 import metadatastore
 import metadatastore.api as mdsc
+import time as ttime
 
 
 class TimeSuite:
@@ -14,13 +15,15 @@ class TimeSuite:
         metadatastore.conf.mds_config['database'] = 'benchmark'
         metadatastore.conf.mds_config['host'] = '127.0.0.1'
         metadatastore.conf.mds_config['port'] = 27017
-        self.d = {}
-        for x in range(500):
-            self.d[x] = None
+        self.config_iters = 1000
+        self.bcfg_time = ttime.time()
+        self.bcfg_params = {'param1': 'value1',
+                            'param2': 'value2'}
 
     def time_keys(self):
-        for key in self.d.keys():
-            pass
+        for _ in xrange(self.config_iters):
+            mdsc.insert_beamline_config(time=self.bcfg_time,
+                                        config_params=self.bcfg_params)
 
     def time_iterkeys(self):
         for key in self.d.iterkeys():
