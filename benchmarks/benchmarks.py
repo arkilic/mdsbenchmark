cfb429898397273a164dc34a4ec02145201373f6
arkilic <arkilic@bnl.gov>
2015-02-23 15:39:04 -0500

    WIP:BeamlineConfig insert test with 1, 10,100,1000,10000 configs

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -3,43 +3,21 @@
 
 import metadatastore
 import metadatastore.api as mdsc
-import time as ttime
 
 
-class TimeSuite:
-    """
-    An example benchmark that times the performance of various kinds
-    of iterating over dictionaries in Python.
-    """
-    def setup(self):
+
+class Suite:
+    
+    params = [1, 10, 100, 1000, 10000, 100000]
+    def setup(self, n):
         metadatastore.conf.mds_config['database'] = 'benchmark'
         metadatastore.conf.mds_config['host'] = '127.0.0.1'
-        metadatastore.conf.mds_config['port'] = 27017
-        self.config_iters = 1000
-        self.bcfg_time = ttime.time()
-        self.bcfg_params = {'param1': 'value1',
-                            'param2': 'value2'}
-
-    def time_keys(self):
-        for _ in xrange(self.config_iters):
-            mdsc.insert_beamline_config(time=self.bcfg_time,
-                                        config_params=self.bcfg_params)
-
-    def time_iterkeys(self):
-        for key in self.d.iterkeys():
-            pass
-
-    def time_range(self):
-        d = self.d
-        for key in range(500):
-            x = d[key]
-
-    def time_xrange(self):
-        d = self.d
-        for key in xrange(500):
-            x = d[key]
-
-
-class MemSuite:
-    def mem_list(self):
-        return [0] * 256
+        metadatastore.conf.mds_config['port'] = 2898
+        self.obj = range(n)
+    
+    def time_bcfg(self, n):
+        for _ in self.obj:
+            mdsc.insert_beamline_config(time=1315315135.5135,
+                                        config_params={'param1': 1})
+    time_bcfg.number = 1
+    time_bcfg.repeat = 1
